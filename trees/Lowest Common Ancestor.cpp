a99585a4fa540f6699e3578dadba051a7c6d6800
sakshamdani <71568789+sakshamdani1402@users.noreply.github.com>
2022-07-13 22:13:59 +0530

    Create Lowest Common Ancestor.cpp

---
+++ trees/Lowest Common Ancestor.cpp
@@ -0,0 +1,19 @@
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+// Output: 6
+// Explanation: The LCA of nodes 2 and 8 is 6.
+
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+// Output: 2
+// Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root->val < p->val && root->val < q->val)  //if root->val smaller than both then search for greater val
+        lowestCommonAncestor(root->right, p, q);
+    if(p->val < root->val && q->val < root->val)  //if root->val > p and q search for smaller val
+        lowestCommonAncestor(root->left, p, q);   //it is in the middle, then it is lowest common ancestor
+
+    return root;
+}
